4a1b3b1cef22e5303acaa1e34c4ab2beee709619
tirunagari1 <38136589+tirunagari1@users.noreply.github.com>
2018-04-13 22:06:22 -0500

    added calci.html calci.css calci.js

---
+++ calci.js
@@ -0,0 +1,25 @@
+function display(i) {
+    form.txt.value = form.txt.value + i;
+}
+
+
+
+
+
+    function ce() {
+        form.txt.value = '';
+
+    }
+
+    function bs() {
+        form.txt.value = form.txt.value.slice(0, form.txt.value.length - 1);
+
+    }
+function e(x) {
+    if(form.txt.value=eval(form.txt.value)){
+
+    }else{
+        form.text.value=eval((form.txt.value)+(form.txt.value));
+    }
+
+}
