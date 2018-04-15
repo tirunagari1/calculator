76dc691c40ef9f5caaf735b6ae8314763b3ac386
tirunagari1 <38136589+tirunagari1@users.noreply.github.com>
2018-04-14 20:16:22 -0500

    updated  opertors functions seperately in calci.js

--- calci.js
+++ calci.js
@@ -1,3 +1,9 @@
+displayNum="";
+storedNum="form.txt.value";
+operation=0;
+queuedOperation=0;
+calculationFinished=false;
+
 function display(i) {
     form.txt.value = form.txt.value + i;
 }
@@ -11,7 +17,36 @@ function display(i) {
 
     }
 function e() {
-    form.txt.value = eval(form.txt.value);
+
+        var textbox = document.getElementById("textbox");
+        displayNum = textbox.value;
+        var evalDisplay = eval(displayNum),
+            evalStored = eval(storedNum);
+
+        if (operation == 1) {
+            displayNum = evalStored + evalDisplay;
+        }
+        else if (operation == 2) {
+            displayNum = evalStored - evalDisplay;
+        }
+        else if (operation == 3) {
+            displayNum = evalStored * evalDisplay;
+        }
+        else if (operation ==4 ) {
+            displayNum = evalStored / evalDisplay;
+        }
+
+        else if (operation ==5 ) {
+            displayNum = evalStored % evalDisplay;
+        }
+    textbox.value = displayNum;
+        if (operation != 0)
+            calculationFinished = true;
+        operation = 0;
+        queuedOperation = 0;
+        displayNum = "";
+        storedNum = "form.txt.value";
+
 
 }
 
@@ -23,4 +58,48 @@ function decimal(dec){
         }
     textbox.value += dec;
 
+}
+
+function setOperation(command) {
+    var textbox = document.getElementById("textbox"),
+        displayNum = textbox .value;
+
+    evalDisplay = eval(displayNum),
+        evalStored = eval(storedNum);
+
+    if (queuedOperation == 0) {
+        storedNum = textbox .value;
+    }
+    else if (queuedOperation == 1) {
+        storedNum = evalStored + evalDisplay;
+    }
+    else if (queuedOperation == 2) {
+        storedNum = evalStored - evalDisplay;
+    }
+    else if (queuedOperation == 3) {
+        storedNum = evalStored * evalDisplay;
+    }
+    else if (queuedOperation == 4) {
+        storedNum = evalStored / evalDisplay;
+    }
+
+    else if (queuedOperation == 5) {
+        storedNum = evalStored % evalDisplay;
+    }
+    if (command == 'add') {
+        operation = 1;
+    }
+    else if (command == 'subtract') {
+        operation = 2;
+    }
+    if (command == 'multiply') {
+        operation = 3;
+    }
+   else if (command == 'division') {
+        operation = 4;
+    } if (command == 'per') {
+        operation = 5;
+    }
+    queuedOperation = operation;
+    textbox.value = '';
 }
