ea723176dbdc91e1aa36c880f831fb9242b6b87f
tirunagari1 <38136589+tirunagari1@users.noreply.github.com>
2018-04-14 19:44:49 -0500

    Division, . no repeat  readonly

--- calci.js
+++ calci.js
@@ -1,11 +1,6 @@
 function display(i) {
     form.txt.value = form.txt.value + i;
 }
-
-
-
-
-
     function ce() {
         form.txt.value = '';
 
@@ -15,11 +10,17 @@ function display(i) {
         form.txt.value = form.txt.value.slice(0, form.txt.value.length - 1);
 
     }
-function e(x) {
-    if(form.txt.value=eval(form.txt.value)){
+function e() {
+    form.txt.value = eval(form.txt.value);
 
-    }else{
-        form.text.value=eval((form.txt.value)+(form.txt.value));
-    }
+}
+
+function decimal(dec){
+    var textbox=document.getElementById("textbox");
+    for(i=0; i< textbox.value.length; i++)
+        if(textbox.value.charAt(i)=='.'){
+            return;
+        }
+    textbox.value += dec;
 
 }
